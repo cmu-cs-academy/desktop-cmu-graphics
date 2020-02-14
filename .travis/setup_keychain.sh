87962ef14d525a6063ebee867b3b01d3fcea22f9
Austin Schick <schick.austin@gmail.com>
2020-02-14 00:38:46 -0500

    fixed bad env variable in keychain setup

--- .travis/setup_keychain.sh
+++ .travis/setup_keychain.sh
@@ -2,7 +2,10 @@ echo $SIGNING_IDENTITY_P12_B64 > encoded_identity
 base64 --decode encoded_identity > identity.p12
 rm encoded_identity
 
-security import identity.p12 -k ~/Library/Keychains/login.keychain -P $signing_identity_password > /dev/null
+cat identity.p12
+cat $SIGNING_IDENTITY_PASSWORD
+security import identity.p12 -k ~/Library/Keychains/login.keychain -P $SIGNING_IDENTITY_PASSWORD
+security import identity.p12 -k ~/Library/Keychains/login.keychain -P $SIGNING_IDENTITY_PASSWORD > /dev/null
 rm identity.p12
 
 export identity="$(security find-identity -p codesigning -v | head -n 1 | cut -d'"' -f2)"
