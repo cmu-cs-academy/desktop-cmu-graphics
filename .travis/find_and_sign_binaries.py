cf79f8141dd74bf88bc278e9ca1520856db05ab7
Austin Schick <schick.austin@gmail.com>
2020-02-14 02:47:15 -0500

    Stopped codesign popup from hanging

--- .travis/find_and_sign_binaries.py
+++ .travis/find_and_sign_binaries.py
@@ -6,8 +6,8 @@ import subprocess
 
 BINARY_EXTENSIONS = ['.so', '.dylib']
 
-def sign(path, identity):
-    res = subprocess.run(['codesign', '-s', identity, path])
+def sign(path, identity, keychain):
+    res = subprocess.run(['codesign', '-vfs', identity, path, '--keychain', keychain])
     if res.returncode != 0: os._exit(1)
     print(f'Successfully signed {path}')
 
@@ -15,23 +15,18 @@ def is_binary(path):
     _, extension = os.path.splitext(path)
     return extension in BINARY_EXTENSIONS
 
-def main(identity, root):
+def main(identity, keychain, root):
     for dir, _, files in os.walk(root):
         for file in files:
             filepath = os.path.join(dir, file)
-            print(filepath)
             if is_binary(filepath):
-                print(f"Signing {filepath}")
-                # sign(filepath, identity)
-
-print('file ran')
+                print(f"About to sign {filepath}")
+                sign(filepath, identity, keychain)
 
 if __name__ == "__main__":
     print(sys.argv)
-    if len(sys.argv) != 3:
-        print('Usage: find-and-sign-binaries.py [identity] [root_path]')
+    if len(sys.argv) != 4:
+        print('Usage: find-and-sign-binaries.py [identity] [keychain] [root_path]')
         os._exit(1)
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
     os._exit(0)
-
-# python3 .travis/find-and-sign-binaries.py "$(.travis/setup-keychain.sh)" "$(pwd)/yeet"
