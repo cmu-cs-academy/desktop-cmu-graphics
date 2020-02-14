af8e8e56c95e04b16b4647bf9ed1b80d3e6292a4
Austin Schick <schick.austin@gmail.com>
2020-02-14 01:40:36 -0500

    Try signing 40 binaries

--- .travis/find_and_sign_binaries.py
+++ .travis/find_and_sign_binaries.py
@@ -16,21 +16,28 @@ def is_binary(path):
     return extension in BINARY_EXTENSIONS
 
 def main(identity, root):
+    maxFiles = 40
+    currFiles = 0
     for dir, _, files in os.walk(root):
         for file in files:
+            currFiles += 1
+            if currFiles > maxFiles:
+                break
             filepath = os.path.join(dir, file)
             print(filepath)
             if is_binary(filepath):
                 sign(filepath, identity)
+        if currFiles > maxFiles:
+            break
 
 print('file ran')
 
-# if __name__ == "__main__":
-#     print(sys.argv)
-#     if len(sys.argv) != 3:
-#         print('Usage: find-and-sign-binaries.py [identity] [root_path]')
-#         os._exit(1)
-#     main(sys.argv[1], sys.argv[2])
-#     os._exit(0)
+if __name__ == "__main__":
+    print(sys.argv)
+    if len(sys.argv) != 3:
+        print('Usage: find-and-sign-binaries.py [identity] [root_path]')
+        os._exit(1)
+    main(sys.argv[1], sys.argv[2])
+    os._exit(0)
 
 # python3 .travis/find-and-sign-binaries.py "$(.travis/setup-keychain.sh)" "$(pwd)/yeet"
