a6eac9ae45e46fa9134b47bbcbf190cf8b721168
Austin Schick <schick.austin@gmail.com>
2020-02-14 02:58:01 -0500

    send set key partition to dev null

--- .travis/setup_keychain.sh
+++ .travis/setup_keychain.sh
@@ -10,7 +10,7 @@ rm encoded_identity
 
 security import identity.p12 -k $KEYCHAIN_NAME -P $SIGNING_IDENTITY_PASSWORD -T /usr/bin/codesign > /dev/null
 rm identity.p12
-security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
+security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN_NAME > /dev/null
 
 export identity="$(security find-identity -p codesigning -v | head -n 1 | cut -d'"' -f2)"
 echo $identity
