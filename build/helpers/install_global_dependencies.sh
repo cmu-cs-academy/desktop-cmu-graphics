95dc1dba831d754361f84a44b4c01c0390be8a22
Austin Schick <schick.austin@gmail.com>
2020-07-06 17:41:55 -0400

    Updated awscli before uploads, have Travis sign before release

--- build/helpers/install_global_dependencies.sh
+++ build/helpers/install_global_dependencies.sh
@@ -4,4 +4,3 @@ brew install cairo || brew upgrade cairo || true
 brew install pyenv || brew upgrade pyenv || true
 brew install wget || brew upgrade wget || true
 python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade awscli
--- build/release.sh
+++ build/release.sh
@@ -5,6 +5,7 @@ export zipname=cmu_graphics_installer.zip
 cd ../
 zip -r $zipname cmu_graphics sample.py
 
+python3 -m pip install --upgrade awscli
 aws s3 cp cmu_graphics_installer.zip $UPLOAD_BUCKET/$zipname
 
 rm cmu_graphics_installer.zip
