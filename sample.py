73a9c11bb7a91e5650e5713f97cf13b0afa0eb9b
Austin Schick <aschick@andrew.cmu.edu>
2020-08-31 12:33:15 -0400

    Update sample to not include solutions

--- sample.py
+++ sample.py
@@ -1,7 +1,3 @@
-# Carnegie Mellon University Computer Science Academy
-# This file is private and provided only to teachers.
-# Do not share, post, or otherwise make this file public.
-
 from cmu_graphics import *
 
 app.background = gradient('white', 'aliceBlue')
@@ -14,17 +10,24 @@ Oval(275, 305, 20, 50, fill='orange', rotateAngle=-50)
 Oval(265, 310, 20, 50, fill='orange', rotateAngle=-20)
 Oval(255, 310, 20, 50, fill='orange', rotateAngle=10)
 
-# body
-Oval(110, 150, 65, 120, rotateAngle=-40)
-Oval(110, 160, 35, 110, fill='white', rotateAngle=-40)
+# wings
+leftWingUp = Group(
+    Oval(110, 150, 65, 120, rotateAngle=-40),
+    Oval(110, 160, 35, 110, fill='white', rotateAngle=-40)
+)
+leftWingDown = Group(
+    Oval(110, 210, 65, 120, rotateAngle=40),
+    Oval(110, 220, 35, 110, fill='white', rotateAngle=40),
+    visible=False
+)
 Oval(290, 150, 65, 120, rotateAngle=40)
 Oval(290, 160, 35, 110, fill='white', rotateAngle=40)
-Oval(200, 150, 150, 230)
 
 # belly
+Oval(200, 150, 150, 230)
 belly = Circle(200, 230, 70, fill='white', border='black', borderWidth=15)
 
-# wings
+# body
 Oval(175, 160, 65, 180, fill='white')
 Oval(225, 160, 65, 180, fill='white')
 Circle(175, 110, 10)
@@ -32,13 +35,14 @@ Circle(225, 110, 10)
 Polygon(185, 130, 215, 130, 205, 150, fill='orange')
 
 # words
-hungryLabel = Label("I'm full!", 320, 50, size=35, visible=False)
+Label('Click To Wave', 200, 370, size=30)
 
 def onMousePress(mouseX, mouseY):
-    # Increase the size of belly to make the penguin puffy.
-    belly.radius += 3
-
-    if (belly.radius > 100):
-        hungryLabel.visible = True
+    if leftWingDown.visible == True:
+        leftWingDown.visible = False
+        leftWingUp.visible = True
+    else:
+        leftWingDown.visible = True
+        leftWingUp.visible = False
 
 cmu_graphics.loop()
