577eefc5822b4832dad34e23822eb6388c73f9fe
Austin Schick <aschick@andrew.cmu.edu>
2020-09-02 15:56:20 -0400

    Fixed style in weather sample

--- samples/weather.py
+++ samples/weather.py
@@ -8,21 +8,33 @@ app.backButton = None
 def celciusToFarenheit(degrees):
     return ((degrees / 5) * 9) + 32
 
+def getAPIData(city):
+    cityData = requests.get(
+        API_BASE + '/api/location/search/?query=' + city
+    ).json()[0]
+    cityWoeid = cityData['woeid']
+    weatherData = requests.get(
+        API_BASE + '/api/location/' + str(cityWoeid) + '/'
+    ).json()['consolidated_weather'][0]
+
+    return weatherData, cityData
+
 def drawWeather(city):
-    cityInfo = requests.get(API_BASE + '/api/location/search/?query=' + city).json()[0]
-    cityWoeid = cityInfo['woeid']
-    weather = requests.get(API_BASE + '/api/location/' + str(cityWoeid) + '/').json()['consolidated_weather'][0]
+    weatherData, cityData = getAPIData(city)
 
     app.group.clear()
     Label('Weather in ', 200, 30, size=40)
-    Label(cityInfo['title'], 200, 75, size=40)
-    Label(weather['weather_state_name'], 200, 130, size=30)
-    imageURL = API_BASE + '/static/img/weather/png/'+ weather['weather_state_abbr'] + '.png'
+    Label(cityData['title'], 200, 75, size=40)
+    Label(weatherData['weather_state_name'], 200, 130, size=30)
+    weather_state = weatherData['weather_state_abbr']
+    imageURL = API_BASE + '/static/img/weather/png/'+ weather_state + '.png'
     Image(imageURL, 200, 230, align='center', width=150, height=150)
 
-    farenheitTemp = celciusToFarenheit(weather['the_temp'])
-    Label(str(pythonRound(farenheitTemp, 2)) + '°F', 25, 350, align='left', size=30)
-    Label('Wind: ' + weather['wind_direction_compass'], 375, 350, align='right', size=30)
+    farenheitTemp = celciusToFarenheit(weatherData['the_temp'])
+    Label(str(pythonRound(farenheitTemp, 2)) + '°F',
+          25, 350, align='left', size=30)
+    Label('Wind: ' + weatherData['wind_direction_compass'],
+          375, 350, align='right', size=30)
 
     app.backButton = Line(80, 30, 20, 30, arrowEnd=True, lineWidth=5)
 
@@ -42,7 +54,7 @@ def onKeyPress(key):
             drawWeather(city)
         except:
             Label('City not found. Try another!', 200, 250, size=20)
-            
+
 def drawText(linesList):
     lineHeight = 30
     lineX = 200 - ((len(linesList) * lineHeight) // 2)
