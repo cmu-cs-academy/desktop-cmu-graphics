9ba77fc59f33fbfad3b10a95e486a13ff16fdc5a
Austin Schick <aschick@andrew.cmu.edu>
2020-09-04 11:51:54 -0400

    Test via tox

--- tests/testall.py
+++ tests/testall.py
@@ -3,11 +3,6 @@ import datetime
 import sys
 import os
 
-PYTHON_PATHS = {
-    'win32': ['C:\Python35', 'C:\Python36', 'C:\Python37', 'C:\Python38'],
-    'darwin': ['~/venv3.5', '~/venv3.6', '~/venv3.7', '~/venv3.8']
-}
-
 def log(*args):
     new_args = [datetime.datetime.now()] + list(args)
     print(*new_args)
@@ -26,23 +21,18 @@ def run_command(cmd):
         log('%s --> %s' % (cmd, resultText))
 
     if result.returncode != 0:
-        log(f'FAILURE OUTPUT FROM: {cmd}')
+        log('FAILURE OUTPUT FROM: %s' % cmd)
         log(resultText)
-        log(f'*** {cmd} FAILED')
+        log('*** %s FAILED' % cmd)
         os._exit(1)
 
 def main():
     os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
-    for python_path in PYTHON_PATHS[sys.platform]:
-        os.environ['PATH'] =  python_path + os.pathsep + os.environ['PATH']
-        log('Running with Python path', python_path)
-        run_command('python3 --version')
-
-        run_command('python3 test_image_gen.py')
-        run_command('python3 test_get_text_input.py')
-        run_command('python3 test_sound.py')
-        run_command('python3 test_autoupdate.py')
+    run_command('python3 test_image_gen.py')
+    run_command('python3 test_get_text_input.py')
+    run_command('python3 test_sound.py')
+    run_command('python3 test_autoupdate.py')
 
 if __name__ == '__main__':
     main()
