8f4f30dec39221a18a68b1d9334db4b7de63aff3
Austin Schick <aschick@andrew.cmu.edu>
2020-09-04 23:19:14 -0400

    Fix syntax error in image gen tests

--- tests/test_image_gen.py
+++ tests/test_image_gen.py
@@ -96,9 +96,9 @@ def run_test(driver, test_name, all_source_code):
         output_path = 'image_gen/%s/output_%d.png' % (test_name, i)
 
         source_code = ''
-        source_code += 'import sys'
-        source_code += 'import os'
-        source_code += 'os.environ["SDL_VIDEODRIVER"] = "dummy"'
+        source_code += '\nimport sys'
+        source_code += '\nimport os'
+        source_code += '\nos.environ["SDL_VIDEODRIVER"] = "dummy"'
         source_code += '\nsys.path.insert(0, "..")'
         source_code += '\nfrom cmu_graphics import *\n'
         source_code += '\n######\n'.join(source_code_pieces[:piece_i])
