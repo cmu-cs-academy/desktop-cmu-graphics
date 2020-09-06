8b216de63dadbb888558eab3f36bfe6dfa56e6cd
Austin Schick <aschick@andrew.cmu.edu>
2020-09-06 11:49:55 -0400

    Manual deploy through script

--- build/release.sh
+++ build/release.sh
@@ -7,7 +7,7 @@ mkdir cmu_graphics_installer
 cp -r cmu_graphics cmu_graphics_installer
 cp samples/* cmu_graphics_installer
 cd cmu_graphics_installer
-zip -r $zipname *
+zip -rq $zipname *
 cd ../
 
 mkdir deploy
@@ -15,7 +15,7 @@ cp cmu_graphics_installer/$zipname deploy
 cp cmu_graphics/meta/version.txt deploy
 rm -r cmu_graphics_installer
 
-if [[ $APPVEYOR ]]; then
-  appveyor PushArtifact deploy/cmu_graphics_installer.zip
-  appveyor PushArtifact deploy/version.txt
-fi
+python3 -m pip install --upgrade awscli
+aws s3 cp deploy/$zipname $UPLOAD_BUCKET/desktop-cmu-graphics/$zipname
+aws s3 cp deploy/version.txt $UPLOAD_BUCKET/desktop-cmu-graphics/version.txt
+rm -r deploy
