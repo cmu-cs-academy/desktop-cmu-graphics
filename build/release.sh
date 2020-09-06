cc61369785becff9a4b3cde574ce85c301085f80
Austin Schick <aschick@andrew.cmu.edu>
2020-09-06 10:43:12 -0400

    Speedup appveyor test

--- build/release.sh
+++ build/release.sh
@@ -14,11 +14,3 @@ mkdir deploy
 cp cmu_graphics_installer/$zipname deploy
 cp cmu_graphics/meta/version.txt deploy
 rm -r cmu_graphics_installer
-
-export tag_version=$(git tag --points-at HEAD)
-export txt_version=$(cat cmu_graphics/meta/version.txt)
-
-if [[ $tag_version != $txt_version ]]; then
-  echo "Git tag does not match version txt"
-  exit 1;
-fi
