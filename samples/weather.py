19a61ba48ecfb2fc4b64e285b38f3e3fb2198ee6
Austin Schick <aschick@andrew.cmu.edu>
2020-11-06 10:35:21 -0500

    Added comments for weather sample

--- samples/weather.py
+++ samples/weather.py
@@ -1,52 +1,74 @@
 import sys
 from cmu_graphics import *
 
+# The base URL for the API we are using: metaweather
 API_BASE = 'https://www.metaweather.com'
-
-app.backButton = None
 
 def celciusToFarenheit(degrees):
     return ((degrees / 5) * 9) + 32
 
+# Given a name of a city, this function returns information about the weather
+# in that city, and information about the city itself
 def getAPIData(city):
+    # First query the API to get information about the city given its name
     cityData = requests.get(
         API_BASE + '/api/location/search/?query=' + city
     ).json()[0]
+
+    # Extract the Where On Earth Identifier from the city information
     cityWoeid = cityData['woeid']
+
+    # Then query the API using the woeid to get its weather
     weatherData = requests.get(
         API_BASE + '/api/location/' + str(cityWoeid) + '/'
     ).json()['consolidated_weather'][0]
 
     return weatherData, cityData
 
+# Given a city, queries the metaweather API to find its weather information,
+# then draws that weather to the canvas
 def drawWeather(city):
+    app.group.clear()
+
     weatherData, cityData = getAPIData(city)
 
-    app.group.clear()
+    # Draws the title "Weather in City: Rainy"
     Label('Weather in ', 200, 30, size=40)
     Label(cityData['title'], 200, 75, size=40)
     Label(weatherData['weather_state_name'], 200, 130, size=30)
+
+    # Given the weather state (sunny, cloudy, windy, etc)
+    # Get an image from metawetaher representing that weather state
     weather_state = weatherData['weather_state_abbr']
     imageURL = API_BASE + '/static/img/weather/png/'+ weather_state + '.png'
     Image(imageURL, 200, 230, align='center', width=150, height=150)
 
+    # Draws the temperature in Farenheit and the wind direction
+    # The temperature returned from metaweather is in Celcius, so we have
+    # to convert it first
     farenheitTemp = celciusToFarenheit(weatherData['the_temp'])
     Label(str(pythonRound(farenheitTemp, 2)) + '°F',
           25, 350, align='left', size=30)
     Label('Wind: ' + weatherData['wind_direction_compass'],
           375, 350, align='right', size=30)
 
-    app.backButton = Line(80, 30, 20, 30, arrowEnd=True, lineWidth=5)
+    # Create a back button so we can return to the search page
+    # app.backButton = Line(80, 30, 20, 30, arrowEnd=True, lineWidth=5)
+    app.backButton.visible = True
 
 def onMousePress(mouseX, mouseY):
-    if app.backButton is not None and app.backButton.hits(mouseX, mouseY):
+    # If the back button is visible (which means we're displaying the weather
+    # for a city right now), and we click it, go back to the search page
+    if app.backButton.visible and app.backButton.hits(mouseX, mouseY):
+        app.backButton.visible = False
         app.group.clear()
-        app.backButton = None
         drawSearchPage()
 
 def drawSearchPage():
     Label('Press S to search for a city.', 200, 200, size=25)
 
+# Search for a city when we press the s key, and try to draw its weather
+# If we can't find the city, print an error message
 def onKeyPress(key):
     if key.lower() == 's':
         city = app.getTextInput('City Name:')
@@ -55,20 +77,36 @@ def onKeyPress(key):
         except:
             Label('City not found. Try another!', 200, 250, size=20)
 
+# A helper function for drawing text centered on the screen
 def drawText(linesList):
     lineHeight = 30
-    lineX = 200 - ((len(linesList) * lineHeight) // 2)
+    # lineY starts out at the center (200) minus half the height of our
+    # whole block of text
+    lineY = 200 - ((len(linesList) * lineHeight) // 2)
     for line in linesList:
-        Label(line, 200, lineX, align='center', size=20)
-        lineX += lineHeight
+        # Create a label for each line, and move lineY down so the next
+        # line is drawn lower
+        Label(line, 200, lineY, align='center', size=20)
+        lineY += lineHeight
 
+# This function loads the third party libraries OpenCV and Pillow, and adds
+# them to the global namespace so we can use them in other functions.
+# We do this in a function so we can handle missing libraries nicely
 def loadModules():
     import requests
     globals()['requests'] = requests
 
+# Try to load the necessary third party libraries, create our back button, and
+# start the application. If loading the libraries fails, print a message about
+# installation
 def start():
     try:
         loadModules()
+        # The visibility of the back button changes when we draw the weather,
+        # and it is accessed in the mouse press handler, so it needs to be
+        # part of the app
+        app.backButton = Line(80, 30, 20, 30, arrowEnd=True,
+                              lineWidth=5, visible=False)
         drawSearchPage()
     except:
         drawText([
