5568ec5f0c08ecf0ad7fb0fea4ab7533f32bde1f
Tate Mauzy <tatenm2@gmail.com>
2021-08-03 18:57:31 -0400

    Move all cairo copying functionality to a Python script for tox Mac tests

---
+++ tests/copy_cairo.py
@@ -0,0 +1,13 @@
+import platform
+import shutil
+
+def main():
+    python_major, python_minor, _ = platform.python_version_tuple()
+    short_pyversion = f"py{str(python_major)}{str(python_minor)}"
+    long_pyversion = f"python{str(python_major)}.{str(python_minor)}"
+    cairo_dir="../cmu_graphics/libs/cairo_loader/modules/cairo_mac/cairo/"
+    tox_lib_dir=f"../.tox/{short_pyversion}/lib/{long_pyversion}/site-packages/cairo"
+    shutil.copytree(cairo_dir, tox_lib_dir)
+
+if __name__ == "__main__":
+    main()
--- tests/print_pyversion_short.py
+++
@@ -1,3 +0,0 @@
-import platform
-python_major, python_minor, _ = platform.python_version_tuple()
-print(f"py{str(python_major)}{str(python_minor)}")
