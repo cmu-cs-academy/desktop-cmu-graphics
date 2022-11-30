4849debd77c2fcaf6c402823f0f5cca64a121e2f
Evan Mallory <evanmall@andrew.cmu.edu>
2022-11-30 15:05:24 -0500

    Add onScreenActivated event

--- tests/image_gen/cs3_screens.py
+++ tests/image_gen/cs3_screens.py
@@ -1,5 +1,20 @@
+calledStart = False
+calledActivated = False
+
+def a_onScreenStart(app):
+    global calledStart
+    assert not calledStart
+    calledStart = True
+
+def a_onScreenActivated(app):
+    global calledActivated
+    assert calledStart
+    calledActivated = True
+
 def a_redrawAll(app):
+    assert calledStart and calledActivated
     drawRect(0,0,200,200,fill='blue')
 
 def b_redrawAll(app):
     drawRect(0,0,200,200,fill='red')
+
