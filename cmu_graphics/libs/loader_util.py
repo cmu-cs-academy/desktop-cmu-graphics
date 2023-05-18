eaccf16d3f3da83dc87cd18ba14638abb6592f83
Austin Schick <aschick@andrew.cmu.edu>
2023-05-18 15:44:51 -0400

    Don't error when programs are run with 3.11, add x86 cairo binaries

--- cmu_graphics/libs/loader_util.py
+++ cmu_graphics/libs/loader_util.py
@@ -20,12 +20,15 @@ def get_platform_string():
     plat += "_%s%s" % (python_major, python_minor)
     return plat
 
+max_minor_version = 11
+
 def verify_os():
     if sys.platform not in ["darwin", "win32"]:
         print("""\
 It looks like your computer is using a(n) %(os)s operating system.
 %(os)s is not currently supported by CMU Graphics. We support Python 3.6
-through Python 3.10 on Windows and MacOS.""" % {'os': sys.platform})
+through Python 3.%(max_minor_version)d on Windows and MacOS.""" 
+% {'os': sys.platform, 'max_minor_version': max_minor_version})
         os._exit(1)
         
 ### END ZIPFILE VERSION ###
@@ -40,16 +43,17 @@ def verify_support():
         print("""\
 It looks like you're running a version of Python 2. Since Python 2 is no
 longer maintaned as of January 1 2020, CMU Graphics does not support Python 2.
-We recommend installing Python 3.10 from python.org""")
+We recommend installing Python 3.%(max_minor_version)d from python.org"""
+% {'max_minor_version': max_minor_version})
         os._exit(1)
 
     ### ZIPFILE VERSION ###
-    if int(python_minor) > 10:
+    if int(python_minor) > max_minor_version:
         print("""\
 It looks like you're running Python 3.%(minor)s. Python 3.%(minor)s is not currently
-supported by CMU Graphics. We support Python 3.6-3.10. We recommend
-installing Python 3.10 from python.org""" %
-{"minor": python_minor})
+supported by CMU Graphics. We support Python 3.6-3.%(max_minor_version)d. We recommend
+installing Python 3.%(max_minor_version)d from python.org""" %
+{"minor": python_minor, 'max_minor_version': max_minor_version})
         os._exit(1)
     ### END ZIPFILE VERSION ###
 
@@ -57,6 +61,6 @@ installing Python 3.10 from python.org""" %
         print("""\
 It looks like you're running Python 3.%(minor)s. Python 3.%(minor)s is not currently
 supported by CMU Graphics. We support Python 3.6 and higher. We recommend 
-installing Python 3.10 from python.org""" %
-{"minor": python_minor})
+installing Python 3.%(max_minor_version)d from python.org""" %
+{"minor": python_minor, 'max_minor_version': max_minor_version})
         os._exit(1)
