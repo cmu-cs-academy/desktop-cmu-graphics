a2b5186f20406f11293eb73a5838dcd9919bd8b7
Evan Mallory <evanmall@andrew.cmu.edu>
2023-10-24 11:02:25 -0400

    Prefer local libraries instead of global libraries when running from zip file

--- cmu_graphics/libs/pil_image_loader/__init__.py
+++ cmu_graphics/libs/pil_image_loader/__init__.py
@@ -5,7 +5,7 @@ from .. import loader_util
 current_directory = os.path.dirname(__file__)
 module_directory = os.path.join(current_directory, 'modules')
 module_path = os.path.join(module_directory, 'pil_' + loader_util.get_platform_string())
-sys.path.append(module_path)
+sys.path.insert(0, module_path)
 
 from PIL import Image
 
