24b45275b13c266616d77dbdb999ef3dfc343c08
Mohamed-Waiel-Shikfa <mshikfa@andrew.cmu.edu>
2024-07-12 09:50:11 +0300

    Minor changes

--- tests/image_gen/openImage_util_func.py
+++ tests/image_gen/openImage_util_func.py
@@ -1,3 +1,2 @@
-from cmu_graphics.utils import openImage
 Image(CMUImage(openImage('sample.jpg')), 0,0, width=400, height=400)
 Image(CMUImage(openImage('https://academy.cs.cmu.edu/static/media/project_10.472f439f.jpg')), 0,0)
