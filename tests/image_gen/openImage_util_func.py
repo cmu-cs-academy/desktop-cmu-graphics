ae705ff55dc9fcb1ecb339999a034779a80e5b22
Mohamed-Waiel-Shikfa <mshikfa@andrew.cmu.edu>
2024-07-13 21:43:14 +0300

    Fixed bug in test, feature is working perfectly, sigh

--- tests/image_gen/openImage_util_func.py
+++ tests/image_gen/openImage_util_func.py
@@ -1,2 +1,2 @@
-Image(CMUImage(openImage('sample.jpg')), 0,0, width=400, height=400)
+Image(CMUImage(openImage('image_gen/sample.jpg')), 0,0, width=400, height=400)
 Image(CMUImage(openImage('https://academy.cs.cmu.edu/static/media/project_10.472f439f.jpg')), 0,0)
