75d5a99a8213fe7913f85c3f46676b033641a8dc
Austin Schick <aschick@andrew.cmu.edu>
2025-03-21 15:18:51 -0400

    Add correct image to cs3_resize_screens

--- tests/test_image_gen.py
+++ tests/test_image_gen.py
@@ -161,7 +161,7 @@ def screenshotAndExit():
             time.sleep(0.01)
         with cmu_graphics.DRAWING_LOCK:
             raw_app.frameworkRedrew = False
-            raw_app.callUserFn("onMousePress", (200,200))
+            raw_app.callUserFn("onMousePress", (200,200,0))
         while not raw_app.frameworkRedrew:
             time.sleep(0.01)
         raw_app.getScreenshot(%s)
