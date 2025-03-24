3d0b225234dcd47bcba22042affae0daa997f3fa
Austin Schick <aschick@andrew.cmu.edu>
2025-03-24 12:29:39 -0400

    Replace Pillow for Python 313t with Pillow for Python 313

--- build/helpers/build_pil_modules.py
+++ build/helpers/build_pil_modules.py
@@ -1,13 +1,16 @@
-# Each link should point to a wheel for this version of the module
 # This script should be run from the pil_image_loader folder
 
 import subprocess
 
+# Each link should point to a wheel for this version of the module
+# Note: Make sure you're not using the `-t` version of these builds.
+# Our guess is that these are for free-threaded Python builds, but if you're
+# not using a free-threaded build, the `-t` binaries don't work.
 links = {
-    'pil_mac_313': 'https://files.pythonhosted.org/packages/79/30/77f54228401e84d6791354888549b45824ab0ffde659bafa67956303a09f/pillow-11.1.0-cp313-cp313t-macosx_10_13_x86_64.whl',
-    'pil_mac_arm_313': 'https://files.pythonhosted.org/packages/ce/b1/56723b74b07dd64c1010fee011951ea9c35a43d8020acd03111f14298225/pillow-11.1.0-cp313-cp313t-macosx_11_0_arm64.whl',
-    'pil_win_32_313': 'https://files.pythonhosted.org/packages/e5/fb/a7960e838bc5df57a2ce23183bfd2290d97c33028b96bde332a9057834d3/pillow-11.1.0-cp313-cp313t-win32.whl',
-    'pil_win_64_313': 'https://files.pythonhosted.org/packages/d7/6c/6ec83ee2f6f0fda8d4cf89045c6be4b0373ebfc363ba8538f8c999f63fcd/pillow-11.1.0-cp313-cp313t-win_amd64.whl',
+    'pil_mac_313': 'https://files.pythonhosted.org/packages/b3/31/9ca79cafdce364fd5c980cd3416c20ce1bebd235b470d262f9d24d810184/pillow-11.1.0-cp313-cp313-macosx_10_13_x86_64.whl',
+    'pil_mac_arm_313': 'https://files.pythonhosted.org/packages/ac/0f/ff07ad45a1f172a497aa393b13a9d81a32e1477ef0e869d030e3c1532521/pillow-11.1.0-cp313-cp313-macosx_11_0_arm64.whl',
+    'pil_win_32_313': 'https://files.pythonhosted.org/packages/33/48/19c2cbe7403870fbe8b7737d19eb013f46299cdfe4501573367f6396c775/pillow-11.1.0-cp313-cp313-win32.whl',
+    'pil_win_64_313': 'https://files.pythonhosted.org/packages/3b/ad/285c556747d34c399f332ba7c1a595ba245796ef3e22eae190f5364bb62b/pillow-11.1.0-cp313-cp313-win_amd64.whl',
 }
 
 def run_command(args):
