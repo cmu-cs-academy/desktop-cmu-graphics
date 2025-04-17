15a5273746f110c5de04d12545b10022ede0cceb
Austin Schick <aschick@andrew.cmu.edu>
2025-04-17 13:12:29 -0400

    Remove temp testing file

--- test_font_warnings.py
+++
@@ -1,7 +0,0 @@
-from cmu_graphics import *
-app.showFontWarnings = True
-Label("Test font warnings", 10, 10, fill='black', font='orbitron', size=20)
-Label("Test font warnings", 10, 10, fill='black', font='caveat', size=20)
-
-
-cmu_graphics.run()
