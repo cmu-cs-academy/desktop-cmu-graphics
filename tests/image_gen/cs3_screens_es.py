c603c10c156c0be4bd8e609f3dc9066f34dd4879
PietroCoppola <69320650+PietroCoppola@users.noreply.github.com>
2025-08-05 22:23:04 -0300

    Update onScreenActivate translation

--- tests/image_gen/cs3_screens_es.py
+++ tests/image_gen/cs3_screens_es.py
@@ -15,29 +15,29 @@ def a_enInicioDeApp(app):
     assert 'b_enInicioDeApp' not in app.function_call_count
     app.function_call_count['a_enInicioDeApp'] += 1
 
-def a_enPantallaActivada(app):
+def a_enActivaciónDePantalla(app):
     assert 'a_enInicioDeApp' in app.function_call_count
     assert 'b_enInicioDeApp' in app.function_call_count
     assert app._app.activeScreen == 'a'
-    app.function_call_count['a_enPantallaActivada'] += 1
+    app.function_call_count['a_enActivaciónDePantalla'] += 1
 
-def b_enPantallaActivada(app):
+def b_enActivaciónDePantalla(app):
     assert app._app.activeScreen == 'b'
     assert app.function_call_count['a_redibujaTodo'] == 2
-    app.function_call_count['b_enPantallaActivada'] += 1
+    app.function_call_count['b_enActivaciónDePantalla'] += 1
 
 def a_enRatónPresionado(app, ratónX, ratónY):
     app.function_call_count['a_enRatónPresionado'] += 1
     establecePantallaActiva('b')
     assert app._app.activeScreen == 'a'
-    assert 'b_enPantallaActivada' not in app.function_call_count
+    assert 'b_enActivaciónDePantalla' not in app.function_call_count
     assert 'b_redibujaTodo' not in app.function_call_count
 
 def a_redibujaTodo(app):
     assert app._app.activeScreen == 'a'
-    assert 'b_enPantallaActivada' not in app.function_call_count
+    assert 'b_enActivaciónDePantalla' not in app.function_call_count
     if app.function_call_count['a_redibujaTodo'] == 0:
-        assert 'a_enPantallaActivada' in app.function_call_count
+        assert 'a_enActivaciónDePantalla' in app.function_call_count
     if app.function_call_count['a_redibujaTodo'] == 1:
         assert 'a_enRatónPresionado' in app.function_call_count
     app.function_call_count['a_redibujaTodo'] += 1
@@ -45,5 +45,5 @@ def a_redibujaTodo(app):
 
 def b_redibujaTodo(app):
     assert app._app.activeScreen == 'b'
-    assert app.function_call_count['b_enPantallaActivada'] == 1
+    assert app.function_call_count['b_enActivaciónDePantalla'] == 1
     dibujaRect(200,200,200,200,relleno='rojo')
