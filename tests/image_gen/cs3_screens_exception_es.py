6cec53fcd3074e3a23da9ad711370344602ca011
PietroCoppola <69320650+PietroCoppola@users.noreply.github.com>
2025-08-08 02:08:03 -0300

    Test missing redrawAll exception

---
+++ tests/image_gen/cs3_screens_exception_es.py
@@ -0,0 +1,16 @@
+def enInicioDeApp(app):
+    pass
+
+def a_enInicioDeApp(app):
+    pass
+
+def b_enInicioDeApp(app):
+    pass
+
+def a_enRatónPresionado(app, mouseX, mouseY):
+    assertRaises(lambda: establecePantallaActiva('b'), "Screen 'b' requires 'b_redibujaTodo()'")
+
+def a_redrawAll(app):
+    dibujaRect(0, 0, 400, 400, fill='verdeBosque')
+    dibujaPolígono(200, 60, 340, 200, 200, 340, 60, 200, fill='oro')
+    dibujaCírculo(200, 200, 70, fill='azulOscuro')
