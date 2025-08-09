2adff9b193d522d7de525cec6bc2636299013ef0
PietroCoppola <69320650+PietroCoppola@users.noreply.github.com>
2025-08-09 01:11:04 -0300

    Add comment

--- tests/image_gen/cs3_screens_exception.py
+++ tests/image_gen/cs3_screens_exception.py
@@ -11,6 +11,8 @@ def a_onMousePress(app, mouseX, mouseY):
     assertRaises(lambda: setActiveScreen('b'), "Screen 'b' requires 'b_redrawAll()'")
 
 def a_redrawAll(app):
+    # Just for funsies, the point of the test is to ensure an exception is raised
+    # when trying to activate a screen without an associated redrawAll definition
     drawRect(0, 0, 400, 400, fill="forestGreen")
     drawPolygon(200, 60, 340, 200, 200, 340, 60, 200, fill='gold')
     drawCircle(200, 200, 70, fill='darkBlue')
