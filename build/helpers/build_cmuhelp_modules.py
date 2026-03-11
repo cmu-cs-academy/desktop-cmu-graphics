3bd022f2e3af32f9f592957b5ff3896c356f139f
sonyasimk <sonya.simkin@gmail.com>
2026-03-11 15:33:45 -0400

    rename directories, add build instructions

--- build/helpers/build_cmuhelp_modules.py
+++ build/helpers/build_cmuhelp_modules.py
@@ -6,9 +6,9 @@ import subprocess
 # Links come from the pypi "download files" page for cmu_graphics_helpers-ce
 
 links = {
-    'cmu_graphics_helpers_mac_315': 'https://test-files.pythonhosted.org/packages/34/09/16d6822bfc42cf762f94be830592f6cc13d54823384d5526fbe0cc52ff6d/cmu_graphics_helpers-0.1.2-cp37-abi3-macosx_10_12_x86_64.whl',
-    'cmu_graphics_helpers_mac_arm_315': 'https://test-files.pythonhosted.org/packages/02/81/62919ee060b3b23aea27cda49871177272793fa89684a162adb257d1b630/cmu_graphics_helpers-0.1.2-cp37-abi3-macosx_11_0_arm64.whl',
-    'cmu_graphics_helpers_win_64_315': 'https://test-files.pythonhosted.org/packages/b0/30/b87cf4286d93f8417643abc8e9a8a5575f4a8358fe865091505fdd49316f/cmu_graphics_helpers-0.1.2-cp37-abi3-win_amd64.whl',
+    'cmu_graphics_helpers_mac_37+': 'fill me in',
+    'cmu_graphics_helpers_mac_arm_37+': 'fill me in',
+    'cmu_graphics_helpers_win_64_37+': 'fill me in',
 }
 
 def run_command(args):
