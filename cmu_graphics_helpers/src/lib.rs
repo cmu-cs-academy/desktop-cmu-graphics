72baf3166e68b2bb432c7fd7247c599c13355287
Austin Schick <aschick@andrew.cmu.edu>
2026-03-18 15:27:21 -0400

    Various Rust refactors

--- cmu_graphics_helpers/src/lib.rs
+++ cmu_graphics_helpers/src/lib.rs
@@ -7,51 +7,55 @@ use geo::BooleanOps;
 use geo::{LineString, MultiPolygon, Polygon};
 
 // type aliases
-type PyPolygon = Vec<Vec<(f64, f64)>>;
+type PyLineString = Vec<(f64, f64)>;
+type PyPolygon = Vec<PyLineString>;
 type PyMultiPolygon = Vec<PyPolygon>;
 
-fn to_poly(poly: &PyPolygon) -> Polygon<f64> {
-    let mut line_strings: Vec<LineString<_>> = poly
-        .clone()
-        .into_iter()
-        .map(|outline| LineString::from(outline))
-        .collect();
-    let exterior = line_strings.remove(0);
-    Polygon::new(exterior, line_strings)
-}
+// conversions from Python to Rust
+fn py_polygon_to_polygon(poly: PyPolygon) -> Polygon<f64> {
+    let mut line_strings = poly.into_iter().map(LineString::from);
 
-fn from_poly(poly: &Polygon<f64>) -> PyPolygon {
-    let holes = poly
-        .interiors()
-        .iter()
-        .map(|hole| hole.points().map(|p| (p.x(), p.y())).collect());
-    let mut exterior = vec![poly.exterior().points().map(|p| (p.x(), p.y())).collect()];
-    exterior.extend(holes);
-    exterior
-}
-
-// takes vector of MultiPolygons, returns union of all of them
-#[pyfunction]
-fn union(ps: Vec<PyMultiPolygon>) -> PyResult<PyMultiPolygon> {
-    if ps.len() < 1 {
-        return Err(PyValueError::new_err(
-            "union must be given at least one MultiPolygon as input",
-        ));
+    match line_strings.next() {
+        Some(exterior) => Polygon::new(exterior, line_strings.collect()),
+        None => Polygon::empty(),
     }
+}
 
-    let multis: Option<MultiPolygon<f64>> = ps
-        .iter()
-        .map(|g| {
-            let p = g.iter().map(to_poly).collect();
-            MultiPolygon::new(p)
-        })
-        .collect::<Vec<MultiPolygon<f64>>>()
+fn py_multi_polygon_to_multi_polygon(multi_poly: PyMultiPolygon) -> MultiPolygon<f64> {
+    let polys = multi_poly.into_iter().map(py_polygon_to_polygon).collect();
+    MultiPolygon::new(polys)
+}
+
+// conversions from Rust to Python
+fn line_string_to_vec(line_string: &LineString<f64>) -> PyLineString {
+    line_string.points().map(|p| (p.x(), p.y())).collect()
+}
+
+fn polygon_to_py_polygon(poly: Polygon<f64>) -> PyPolygon {
+    let holes = poly.interiors().iter().map(line_string_to_vec);
+    let exterior = line_string_to_vec(poly.exterior());
+
+    std::iter::once(exterior).chain(holes).collect()
+}
+
+fn multi_polygon_to_py_multi_polygon(multi_poly: MultiPolygon<f64>) -> PyMultiPolygon {
+    multi_poly.into_iter().map(polygon_to_py_polygon).collect()
+}
+
+// Unions a vector of PyMultiPolygons
+#[pyfunction]
+fn union(py_polys: Vec<PyMultiPolygon>) -> PyResult<PyMultiPolygon> {
+    let union_result_or_none = py_polys
         .into_iter()
+        .map(py_multi_polygon_to_multi_polygon)
         .reduce(|u, m| m.union(&u));
 
-    let output: PyMultiPolygon = multis.unwrap().0.iter().map(from_poly).collect();
-
-    Ok(output)
+    match union_result_or_none {
+        Some(union_result) => Ok(multi_polygon_to_py_multi_polygon(union_result)),
+        None => Err(PyValueError::new_err(
+            "union must be given at least one MultiPolygon as input",
+        )),
+    }
 }
 
 #[pymodule]
